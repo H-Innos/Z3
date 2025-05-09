085374afd920b4596cfeba3a977dc063c0b4965a
H_Innos <henrik.innos@gmail.com>
2025-05-09 18:40:07 +0300

    added comments

--- constants.py
+++ constants.py
@@ -1,5 +1,6 @@
 from z3 import BitVecVal
 
+# Some tests take a very long time to run on 32-bit numbers, can be set to 32 when running specific tests (UINT_MAX and INT_MAX also need to be updated)
 BIT_VECTOR_LENGTH = 16 #32
 ONE = BitVecVal(0x1, BIT_VECTOR_LENGTH)
 ZERO = BitVecVal(0x0, BIT_VECTOR_LENGTH)
