28a835e47b4bbe0dc3e6119121aa666ca6b18691
Henrik Innos <henrik.innos@gmail.com>
2025-05-11 03:23:06 +0300

    updated logxor proof to more precise one

--- intervals/test_logxor.py
+++ intervals/test_logxor.py
@@ -1,4 +1,4 @@
-from z3 import Solver, BitVecs, BV2Int, Not, And, unsat, Abs
+from z3 import Solver, BitVecs, BV2Int, Not, And, unsat, Abs, BitVecVal, IntVal
 
 from constants import BIT_VECTOR_LENGTH
 from helpers import max_val_bit_constrained, Max, Min, min_val_bit_constrained
@@ -54,7 +54,8 @@ def test_logxor_otherwise():
     ly, uy = initialize_interval(s, y, "y")
 
     xor = BV2Int(x ^ y, True)
-    lower = Min(Min(min_val_bit_constrained(Abs(lx)+1), min_val_bit_constrained(Abs(ux)+1)), Min(min_val_bit_constrained(Abs(ly)+1), min_val_bit_constrained(Abs(uy)+1)))
+    lower = Min(Min(min_val_bit_constrained(lx),min_val_bit_constrained(ly)),
+                Min(BV2Int(-max_val_bit_constrained(ux)-1, True),BV2Int(-max_val_bit_constrained(uy)-1, True)))
     upper = BV2Int(Max(Max(max_val_bit_constrained(lx), max_val_bit_constrained(ux)), Max(max_val_bit_constrained(ly), max_val_bit_constrained(uy))), True)
 
     s.add(Not(And(lower <= xor, xor <= upper)))
